1b5c946349a416d67c4424b0b8c90357cff11ceb
Mike-0-code <avila.miguel1405@gmail.com>
2026-04-16 17:48:49 -0600

    Update gameState.js

--- js/gameState.js
+++ js/gameState.js
@@ -1,4 +1,44 @@
-// Estados posibles
+const State = {
+    PLAYING: 'playing',
+    VICTORY: 'victory',
+    DEFEAT: 'defeat'
+};
+
+class GameStateManager {
+    constructor() {
+        this.state = State.PLAYING;
+        this.listeners = [];
+    }
+    
+    getState() {
+        return this.state;
+    }
+    
+    setState(newState) {
+        this.state = newState;
+        this.notifyListeners();
+    }
+    
+    addListener(callback) {
+        this.listeners.push(callback);
+    }
+    
+    notifyListeners() {
+        this.listeners.forEach(cb => cb(this.state));
+    }
+    
+    isPlaying() {
+        return this.state === State.PLAYING;
+    }
+    
+    isVictory() {
+        return this.state === State.VICTORY;
+    }
+    
+    isDefeat() {
+        return this.state === State.DEFEAT;
+    }
+}// Estados posibles
 const GameState = {
   MENU: 'menu',           // selección de dificultad (no usado realmente)
   PLAYING: 'playing',     // partida activa

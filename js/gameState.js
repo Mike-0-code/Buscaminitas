c545e6c56df65692c60cf80bcda3e07c036c6ea8
Mike-0-code <avila.miguel1405@gmail.com>
2026-04-16 17:46:25 -0600

    Update gameState.js

--- js/gameState.js
+++ js/gameState.js
@@ -1,1 +1,7 @@
-
+// Estados posibles
+const GameState = {
+  MENU: 'menu',           // selección de dificultad (no usado realmente)
+  PLAYING: 'playing',     // partida activa
+  VICTORY: 'victory',     // victoria - muestra animación
+  DEFEAT: 'defeat'        // derrota - muestra minas
+};
